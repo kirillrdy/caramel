6e5b8e6340267ca69a73bbf861b247b84929111f
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-01-18 14:56:32 +1100

    refactored NotWrapper into Proxy Added in? method

--- lib/caramel.rb
+++ lib/caramel.rb
@@ -1,5 +1,6 @@
 module Caramel
-  class NotWrapper
+
+  class ProxyWrapper
     attr_accessor :parent
 
     def initialize parent
@@ -7,21 +8,59 @@ module Caramel
     end
 
     def method_missing(*args)
-      result = parent.send(*args)
-      return !result
+      @result = parent.send(*args)
+      return modify
     end
 
+    def modify
+      @result
+    end
+
+  end
+
+  # Intentionaly left blank
+  class IsWrapper < ProxyWrapper
+  end
+
+  class NotWrapper < ProxyWrapper
+    def modify
+      !@result
+    end
   end
 
 end
 
 class Object
 
-  def not
-    Caramel::NotWrapper.new self
+  # if person.is 'John'
+  def is(*args)
+    if args.first
+      self == args.first
+    else
+      Caramel::IsWrapper.new self
+    end
+  end
+
+  # if my_varliable.is_not.empty?
+  # if 'Jack'.is_not 'John'
+  def not(*args)
+    if args.first
+      self != args.first
+    else
+      Caramel::NotWrapper.new self
+    end
   end
 
   alias :is_not :not
   alias :are_not :not
+
+
+  # if 'apple'.in? @list_of_fruits
+  def in? array
+    array.include? self
+  end
+  alias :in :in?
+  alias :is_in? :in?
+  alias :is_in :in?
 
 end
